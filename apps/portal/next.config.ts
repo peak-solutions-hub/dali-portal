a717b99a776f4ce5cb7effab28e5a959086a2cb4
okjose <joseearronn@gmail.com>
2025-12-18 20:11:50 +0800

    setup: add dockerfiles for the apps (#7)
    * ci: add lint-build workflow
    
    * ci: use node v22 or higher for lts
    
    * feat: add dockerfile for each app
    
    * ci: update backend dockerfile

--- apps/portal/next.config.ts
+++ apps/portal/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	transpilePackages: ["@repo/ui"],
+	output: "standalone",
 };
 
 export default nextConfig;
