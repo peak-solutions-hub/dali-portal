27ae48112cafb36f4bac29346bbf5e0d8ad005d3
lavefayt <102245874+lavefayt@users.noreply.github.com>
2026-01-07 18:01:04 +0800

    feat: added the about-section

--- apps/portal/src/components/home/index.ts
+++ apps/portal/src/components/home/index.ts
@@ -1,3 +1,4 @@
+export { AboutSection } from "./about-section";
 export { HeroSection } from "./hero-section";
 export { QuickActions } from "./quick-actions";
 export { RecentUpdates } from "./recent-updates";
