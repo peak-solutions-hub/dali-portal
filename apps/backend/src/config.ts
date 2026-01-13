9926222dc992fa1e24dd219107af6e57600ce992
Jose <joseearronn@gmail.com>
2026-01-13 12:09:33 +0800

    chore: add CORS config

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,7 +1,10 @@
 export default () => ({
-	port: parseInt(process.env.PORT ?? 8080, 10),
+	port: parseInt(process.env.PORT ?? "8080", 10),
 	database: {
 		url: process.env.DATABASE_URL,
 	},
-	// add config values/envs here
+	// env looks like "http://localhost:3000,http://localhost:3001"
+	corsOrigins: process.env.CORS_ORIGINS?.split(",").map((origin) =>
+		origin.trim(),
+	) ?? ["http://localhost:3000", "http://localhost:3001"],
 });
