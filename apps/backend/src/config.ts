039cb3ab37892d1931ab40d8ffeca9ef5bd9c3c3
mich_constantino <mjconstantino12345@gmail.com>
2026-01-18 20:43:15 +0800

    feat: added user management(administrative tools) (#24)
    * feat: added initial user-managment page and components
    
    * feat: added sonner notifications when doing actions
    
    * feat: added api routes for the user and role tables and connected it to the frontend components
    
    * feat: added user invite by email
    
    * refactor: removed pagination logic and improved clinet side search and filter
    
    * feat: added logic where if there were no changes made when updating a user, api will not be called
    
    * chore: added warning and maxlength validation to inputs in the dialogs
    
    * fix: added middle truncation for the long email edge cases and fixed overflow in delete dialog
    
    * fix: fix deactivate user logic include invited status users

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -6,4 +6,9 @@ export default () => ({
 	// env looks like "http://localhost:3000,http://localhost:3001"
 	corsOrigins:
 		process.env.CORS_ORIGINS ?? "http://localhost:3000,http://localhost:3001",
+	supabase: {
+		url: process.env.SUPABASE_URL,
+		serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+	},
+	adminUrl: process.env.ADMIN_URL ?? "http://localhost:3001",
 });
